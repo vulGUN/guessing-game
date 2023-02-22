bec1caf2137f65ea349be0bbcb4ae5b68994b983
Max <likha.max@gmail.com>
2023-02-22 16:09:49 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = null;
+    this.max = null;
+    this.candidate = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    this.candidate = Math.round((this.min + this.max) / 2);
+    return this.candidate;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.candidate;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.candidate;
+  }
 }
 
 module.exports = GuessingGame;
